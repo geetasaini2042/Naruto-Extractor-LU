be18a954305b3297b1029c8cd5a31a2ec052ea79
geetasaini2042 <Geetasaini2042@gmail.com>
2025-10-27 20:45:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "7472777466:AAHdCsYtZpPnoMv67OL5_Hv2yjvy2Nz8jEg"
+    BOT_TOKEN = "8292521812:AAEa579lLytzdpAlj_1pwCMMg5QCO2WaPBs"
     API_ID = 22581733
     API_HASH = "1db7bdcf908100cc641c6a5276765c3d"
-    ADMIN = '6530997270'.split(',')
+    ADMIN = '6150091802'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
     DB_URL = "mongodb+srv://neoayush444:3kuwGf9jKicbDxvT@cluster0.f9vq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DB_NAME = "Crack_Jack"
-    TXT_LOG = -1002294532244
-    AUTH_LOG = -1002294532244
-    HIT_LOG = -1002294532244
-    DRM_DUMP = -1002294532244
-    CHANNEL = --1002487777733
+    TXT_LOG = -1003206843368
+    AUTH_LOG = -1003206843368
+    HIT_LOG = -1003206843368
+    DRM_DUMP = -1003206843368
+    CHANNEL = -1003206843368
     CH_URL = "https://t.me/justfortestinggwak"
     OWNER = "https://t.me/mrcraacker"
     THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
